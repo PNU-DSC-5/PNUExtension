ac833faa899db47c2e16e5e54514cbade4a0d1c9
Rina <computer7214@gmail.com>
2020-10-26 21:53:02 +0900

    Timer 구현 완료
    Component 분리 필요
    시간 선택 사항 확장 해야함

--- client/src/components/appbar/AppBar.tsx
+++ client/src/components/appbar/AppBar.tsx
@@ -14,8 +14,13 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import TextField from '@material-ui/core/TextField';
+//import DialogContentText from '@material-ui/core/DialogContentText';
+//import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 // styles
 import { createStyles, makeStyles, Theme, fade  } from '@material-ui/core/styles';
@@ -51,7 +56,15 @@ const useStyles = makeStyles((theme: Theme) =>
       display: 'flex',
       flexGrow: 0,
       paddingLeft: '10px'
-    }
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+
   }),
 );
 
@@ -100,24 +113,64 @@ export function LayoutAppBar(): JSX.Element {
     }
 
     const remainTime = ({ remainingTime } : any) => {
+      currentTime = remainingTime
       const hours = Math.floor(remainingTime / 3600)
       const minutes = Math.floor((remainingTime % 3600) / 60)
       const seconds = remainingTime % 60
-      let time;
-      if (hours === 0 && minutes === 0) time = `${seconds}`
-      else if (hours === 0) time = `${minutes}:${seconds}`
-      else time = `${hours}:${minutes}`
+      let minutesStr: string = `${minutes}`;
+      let secondsStr: string = `${seconds}`;
+      if (minutes < 10) minutesStr = '0' + minutes ;
+      if (seconds < 10) secondsStr = '0' + seconds;
+      let resultTime;
+      if (hours === 0 && minutes === 0) resultTime = `${secondsStr}`
+      else if (hours === 0) resultTime = `${minutesStr}:${secondsStr}`
+      else resultTime = `${hours}:${minutesStr}`
     
-      return time
+      return resultTime
     }
 
     const [open, setOpen] = React.useState(false);
+    let [settingTime, setSettingTime] = React.useState(0);
+    let [currentTime, setCurrentTime] = React.useState(0);
+    const [isPlay, setIsPlay] = React.useState(false);
+    const [selectTime_hours, setSelectTime_hours] = React.useState(0);
+    const [selectTime_minutes, setSelectTime_minutes] = React.useState(0);
+    const [selectTime_seconds, setSelectTime_seconds] = React.useState(0);
+    //let currentTime = 0;
 
     const handleClickOpen = () => {
       setOpen(true);
+      setCurrentTime(currentTime);
     };
-    const handleClose = () => {
+    const handleCancel = () => {
       setOpen(false);
+      setCurrentTime(currentTime);
+    };
+    const handleReset = () => {
+      setSettingTime(0);
+      setCurrentTime(0);
+    };
+    const handleSet = () => {
+      setOpen(false);
+      setIsPlay(true);
+      const selectTime = selectTime_hours * 3600 + selectTime_minutes * 60 + selectTime_seconds;
+      setSettingTime(selectTime);
+      setCurrentTime(selectTime);
+    };
+    const handleChangeHours = (event: React.ChangeEvent<{ value: unknown }>) => {
+      //setSettingTime(event.target.value as number);
+      setSelectTime_hours(event.target.value as number);
+      setCurrentTime(currentTime);
+    };
+    const handleChangeMinutes = (event: React.ChangeEvent<{ value: unknown }>) => {
+      //setSettingTime(event.target.value as number);
+      setSelectTime_minutes(event.target.value as number);
+      setCurrentTime(currentTime);
+    };
+    const handleChangeSeconds = (event: React.ChangeEvent<{ value: unknown }>) => {
+      //setSettingTime(event.target.value as number);
+      setSelectTime_seconds(event.target.value as number);
+      setCurrentTime(currentTime);
     };
 
     const CountdownTimer = (): JSX.Element => {
@@ -128,11 +181,12 @@ export function LayoutAppBar(): JSX.Element {
             style={{backgroundColor: 'white', borderRadius: 50, width:50, height: 60}} 
             onClick={handleClickOpen}
           >
-            <CountdownCircleTimer
-              isPlaying
+            <CountdownCircleTimer 
+              isPlaying={isPlay}
               size={50}
-              duration={10}
-              //initialRemainingTime={5}
+              strokeWidth={5}
+              duration={settingTime}
+              initialRemainingTime={currentTime}
               trailColor={"#FFFFFF"}
               colors={"#FF0000"}
             >
@@ -140,20 +194,62 @@ export function LayoutAppBar(): JSX.Element {
               {/* {{({ remainingTime }) => remainingTime}} */}
             </CountdownCircleTimer>
           </Button>
-          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">시간을 설정해주세요(미완성)</DialogTitle>
+          <Dialog open={open} onClose={handleCancel} aria-labelledby="form-dialog-title">
+            {/* {<DialogTitle id="form-dialog-title">시간을 설정해주세요</DialogTitle>} */}
             <DialogContent>
             <CountdownCircleTimer
               isPlaying
-              size={50}
-              duration={10}
-              //initialRemainingTime={5}
+              size={400}
+              duration={settingTime}
+              initialRemainingTime={currentTime}
               trailColor={"#FFFFFF"}
               colors={"#FF0000"}
             >
               {remainTime}
               {/* {{({ remainingTime }) => remainingTime}} */}
             </CountdownCircleTimer>
+            <FormControl className={classes.formControl}>
+            <InputLabel id="hours_select_label">Hours</InputLabel>
+              <Select
+                labelId="hours_select_label"
+                id="hours_select"
+                value={selectTime_hours}
+                onChange={handleChangeHours}
+              >
+                <MenuItem value={0}>0</MenuItem>
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+            <InputLabel id="minutes_select_label">Minutes</InputLabel>
+              <Select
+                labelId="minutes_select_label"
+                id="minutes_select"
+                value={selectTime_minutes}
+                onChange={handleChangeMinutes}
+              >
+                <MenuItem value={0}>0</MenuItem>
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+            <InputLabel id="seconds_select_label">Seconds</InputLabel>
+              <Select
+                labelId="seconds_select_label"
+                id="seconds_select"
+                value={selectTime_seconds}
+                onChange={handleChangeSeconds}
+              >
+                <MenuItem value={0}>0</MenuItem>
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={20}>20</MenuItem>
+                <MenuItem value={30}>30</MenuItem>
+              </Select>
+            </FormControl>
               {/* {<TextField
                 autoFocus
                 margin="dense"
@@ -164,10 +260,10 @@ export function LayoutAppBar(): JSX.Element {
               />} */}
             </DialogContent>
             <DialogActions>
-              <Button onClick={handleClose} color="primary">
-                Cancel
+              <Button onClick={handleReset} color="primary">
+                Reset
               </Button>
-              <Button onClick={handleClose} color="primary">
+              <Button onClick={handleSet} color="primary">
                 Set
               </Button>
             </DialogActions>
