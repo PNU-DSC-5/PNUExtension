606518042a93c26a58101ab1c01f9c6a0bb72272
KIMBEOBWOO <qjqdn1568@naver.com>
2020-10-27 15:10:45 +0900

    prettier 적용 필요시 커스텀

--- client/src/components/main-page/top-section/TopSectionTest.tsx
+++ client/src/components/main-page/top-section/TopSectionTest.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 // material-ui core components
-import { Typography, Button, Grid, TextField, InputAdornment ,FormControl, OutlinedInput, IconButton} from '@material-ui/core';
+import {
+  Typography,
+  Button,
+  Grid,
+  TextField,
+  InputAdornment,
+  FormControl,
+  OutlinedInput,
+  IconButton,
+} from '@material-ui/core';
 
 // styles
-import { createStyles, makeStyles, Theme, } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 // hooks
 import useEventTargetValue from '../../../utils/hooks/useEventTargetValue';
@@ -16,89 +25,95 @@ import SearchIcon from '@material-ui/icons/Search';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     textField: {
-        '& label.Mui-focused': {
-          color: 'white',
-        },
-        '& .MuiInput-underline:after': {
-          borderBottomColor: 'white',
-        },
-        '& .MuiOutlinedInput-root': {
-          '& fieldset': {
-            borderColor: theme.palette.secondary.light,
-          },
-          '&:hover fieldset': {
-            borderColor: 'white',
-          },
-          '&.Mui-focused fieldset': {
-            borderColor: 'white',
-          },
-        },
-        width: '700px',
-        height: 'auto',     
+      '& label.Mui-focused': {
+        color: 'white',
       },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: 'white',
+      },
+      '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: theme.palette.secondary.light,
+        },
+        '&:hover fieldset': {
+          borderColor: 'white',
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: 'white',
+        },
+      },
+      width: '700px',
+      height: 'auto',
+    },
     searchBackground: {
-      "&:hover": {
+      '&:hover': {
         backgroundColor: theme.palette.primary.light,
-      }
-    }
+      },
+    },
   }),
 );
 
 export default function TopTest(): JSX.Element {
-    const classes = useStyles();
-    const {value, handleChange} = useEventTargetValue();
-    const history = useHistory();
-    const re = /[\u3131-\uD79D]/ugi;
+  const classes = useStyles();
+  const { value, handleChange } = useEventTargetValue();
+  const history = useHistory();
+  const re = /[\u3131-\uD79D]/giu;
 
-    // const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
-    // korean.test(string);
+  // const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+  // korean.test(string);
 
-    const handleSearch = () => {
-      console.log(value);
-      if(value.length > 0)
-        window.location.replace('https://www.google.com/search?q='+value.match(re))
-    }
+  const handleSearch = () => {
+    console.log(value);
+    if (value.length > 0)
+      window.location.replace(
+        'https://www.google.com/search?q=' + value.match(re),
+      );
+  };
 
-    const handleEnterKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
-      if(e.key === 'Enter' && value.length > 0) 
-        window.location.replace('https://www.google.com/search?q='+escape(value))
-    }
+  const handleEnterKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'Enter' && value.length > 0)
+      window.location.replace(
+        'https://www.google.com/search?q=' + escape(value),
+      );
+  };
 
-    return(
-        <Grid container direction="column" style={{ marginTop: '60px' }}>
-            <Typography color="secondary" variant="h4">
-                Top Section
-            </Typography>
+  return (
+    <Grid container direction="column" style={{ marginTop: '60px' }}>
+      <Typography color="secondary" variant="h4">
+        Top Section
+      </Typography>
 
-            <FormControl className={classes.textField} style={{ alignSelf: 'center', marginTop: '60px' }}>
-              <OutlinedInput
-                value={value}
-                onChange={handleChange}
-                endAdornment={ 
-                  <InputAdornment position="end">
-                    <IconButton 
-                      onClick={handleSearch} 
-                      color="secondary"
-                      className={classes.searchBackground}
-                    >
-                      <SearchIcon fontSize="large" style={{color: 'white'}}/>
-                    </IconButton>
-                  </InputAdornment>
-                }
-                inputProps={{
-                  style: {
-                    textAlign: 'center',
-                    fontFamily: 'AppleSDGothicNeo',
-                    fontSize: '35px',
-                    fontWeight: 440,
-                    color: '#ffff'
-                  },
-                }}
-                onKeyPress={handleEnterKeyPress}
-              />
-                 
-            </FormControl>
-            {/* <TextField 
+      <FormControl
+        className={classes.textField}
+        style={{ alignSelf: 'center', marginTop: '60px' }}
+      >
+        <OutlinedInput
+          value={value}
+          onChange={handleChange}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                onClick={handleSearch}
+                color="secondary"
+                className={classes.searchBackground}
+              >
+                <SearchIcon fontSize="large" style={{ color: 'white' }} />
+              </IconButton>
+            </InputAdornment>
+          }
+          inputProps={{
+            style: {
+              textAlign: 'center',
+              fontFamily: 'AppleSDGothicNeo',
+              fontSize: '35px',
+              fontWeight: 440,
+              color: '#ffff',
+            },
+          }}
+          onKeyPress={handleEnterKeyPress}
+        />
+      </FormControl>
+      {/* <TextField 
                 variant="outlined" 
                 value={value}
                 onChange={handleChange}
@@ -124,11 +139,10 @@ export default function TopTest(): JSX.Element {
                 }}
                 className={classes.textField}
             >
-              <IconButton>
+              <IconButton> 
                   <SearchIcon fontSize="large"/>
               </IconButton>
             </TextField> */}
-        </Grid>
-        
-    );
+    </Grid>
+  );
 }
