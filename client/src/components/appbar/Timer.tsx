a423d0168f7e25b55e12b0634fe2d4cd9efdf6c2
RINA :D <computer7214@gmail.com>
2020-11-27 00:14:43 +0900

    Timer 정보 localStorage에 저장
    Timer에 쓰일 정보인 startTime과 settingTime을 localStorage에 저장했습니다. -> 새로고침이나 브라우저를 껐다켜도 저장됩니다.

--- client/src/components/appbar/Timer.tsx
+++ client/src/components/appbar/Timer.tsx
@@ -11,7 +11,7 @@ import Input from '@material-ui/core/Input';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 // countdown timer
-import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -19,22 +19,21 @@ const useStyles = makeStyles((theme: Theme) =>
       position: 'absolute',
       width: 500,
       backgroundColor: 'white',
-      borderRadius: '10px'
+      borderRadius: '10px',
     },
     clock: {
-      margin: 35
+      margin: 35,
     },
     inputContainer: {
       display: 'flex',
       flexGrow: 0,
       marginBottom: 10,
-      float: 'right'
+      float: 'right',
     },
   }),
 );
 
 export default function Timer(): JSX.Element {
-
   const classes = useStyles();
 
   function getModalStyle() {
@@ -48,68 +47,119 @@ export default function Timer(): JSX.Element {
     };
   }
 
-  const remainTime = ({ remainingTime }: any) => { //남은 시간 출력
-    currentTime = remainingTime
-    const hours = Math.floor(remainingTime / 3600)
-    const minutes = Math.floor((remainingTime % 3600) / 60)
-    const seconds = remainingTime % 60
-    let minutesStr: string = `${minutes}`;
-    let secondsStr: string = `${seconds}`;
-    if (minutes < 10) minutesStr = '0' + minutes;
-    if (seconds < 10) secondsStr = '0' + seconds;
-    let resultTime;
-    if (hours === 0 && minutes === 0) resultTime = `${secondsStr}` //1분 미만이면 초만 출력
-    else if (hours === 0) resultTime = `${minutesStr}:${secondsStr}` //1시간 미만이면 분:초 출력
-    else resultTime = `${hours}:${minutesStr}` //1시간 이상이면 시:분 출력
+  const remainTime = () => {
+    //남은 시간 출력
+    const startTimeStr = localStorage.getItem('startTime');
+    const settingTimeStr = localStorage.getItem('settingTime');
+    if (startTimeStr && settingTimeStr) {
+      const startTime = JSON.parse(startTimeStr);
+      const settingTime = JSON.parse(settingTimeStr);
 
-    return resultTime
-  }
+      const date = new Date();
+
+      const remainingTime = (startTime + settingTime - date.getTime()) / 1000;
+
+      const hours = Math.floor(remainingTime / 3600);
+      const minutes = Math.floor((remainingTime % 3600) / 60);
+      const seconds = Math.floor(remainingTime % 60);
+      let minutesStr: string = `${minutes}`;
+      let secondsStr: string = `${seconds}`;
+      if (minutes < 10) minutesStr = '0' + minutes;
+      if (seconds < 10) secondsStr = '0' + seconds;
+      let resultTime;
+
+      //시간이 끝났을 때 0 출력
+      if (remainingTime <= 0) return '0';
+      //1분 미만이면 ss만 출력
+      else if (hours === 0 && minutes === 0) resultTime = `${secondsStr}`;
+      //1시간 미만이면 mm:ss 출력
+      else if (hours === 0) resultTime = `${minutesStr}:${secondsStr}`;
+      //1시간 이상이면 hh:mm 출력
+      else resultTime = `${hours}:${minutesStr}`;
+
+      return resultTime;
+    }
+  };
+
+  const getSettingTime = () => {
+    const dataString = localStorage.getItem('settingTime');
+    if (dataString) {
+      const settingTime = JSON.parse(dataString);
+      return settingTime / 1000; //총 설정한 시간 가져옴 : s 단위
+    }
+  };
+
+  const getRemaingTime = () => {
+    const startTimeStr = localStorage.getItem('startTime');
+    const settingTimeStr = localStorage.getItem('settingTime');
+    if (startTimeStr && settingTimeStr) {
+      const startTime = JSON.parse(startTimeStr);
+      const settingTime = JSON.parse(settingTimeStr);
+      let date = new Date();
+
+      let remainingTime = (startTime + settingTime - date.getTime()) / 1000;
+      return remainingTime; //남은 시간 가져옴 : s 단위
+    }
+  };
 
   const [open, setOpen] = React.useState(false);
-  let [settingTime, setSettingTime] = React.useState(0); //설정한 총 시간
-  let [currentTime, setCurrentTime] = React.useState(0); //설정한 시간 중 남은 시간
-  const [selectTime_minutes, setSelectTime_minutes] = React.useState(0); //설정할 시간 : 분
+  const [selectTime_minutes, setSelectTime_minutes] = React.useState(0); //설정할 시간 : m 단위
   const [modalStyle] = React.useState(getModalStyle); //팝업 스타일 지정
 
-  const handleClickOpen = () => { //Timer를 클릭하면 실행되는 함수
-    setCurrentTime(currentTime); //현재 남은 시간 저장
+  const handleClickOpen = () => {
+    //Timer를 클릭하면 실행되는 함수
     setOpen(true); //팝업을 엶
   };
-  const handleCancel = () => { //Dialog 바깥을 누르면 실행되는 함수
-    setCurrentTime(currentTime); //현재 남은 시간 저장
+  const handleCancel = () => {
+    //Dialog 바깥을 누르면 실행되는 함수
     setOpen(false); //팝업을 닫음
   };
-  const handleReset = () => { //Dialog에서 Reset 버튼을 누르면 실행되는 함수
-    setSettingTime(0); //설정한 시간 초기화
-    setCurrentTime(0); //현재 남은 시간 초기화
+  const handleReset = () => {
+    //Dialog에서 Reset 버튼을 누르면 실행되는 함수
+    localStorage.setItem('startTime', JSON.stringify(0));
+    localStorage.setItem('settingTime', JSON.stringify(0));
   };
-  const handleSet = () => { //Dialog에서 Set 버튼을 누르면 실행되는 함수
+  const handleSet = () => {
+    //Dialog에서 Set 버튼을 누르면 실행되는 함수
     setOpen(false); //팝업을 닫음
     const selectTime = selectTime_minutes * 60; //입력받은 시간을 통해 Timer에 설정할 시간 계산
-    setSettingTime(selectTime);
-    setCurrentTime(selectTime);
+
+    let now = new Date();
+    localStorage.setItem('startTime', JSON.stringify(now.getTime())); //set을 누른 시각 : ms 단위
+    localStorage.setItem(
+      'settingTime',
+      JSON.stringify(selectTime_minutes * 60 * 1000),
+    ); //총 설정 시간 : ms 단위
   };
-  const handleChangeMinutes = (event: React.ChangeEvent<{ value: unknown }>) => { //분을 설정하면 실행되는 함수
+
+  const handleChangeMinutes = (
+    event: React.ChangeEvent<{ value: unknown }>,
+  ) => {
+    //분을 설정하면 실행되는 함수
     setSelectTime_minutes(event.target.value as number);
-    setCurrentTime(currentTime);
   };
 
   const CountdownTimer = (): JSX.Element => {
     return (
       <div>
         <IconButton //타이머 버튼
-          style={{ backgroundColor: 'white', borderRadius: 30, height: 40, margin: 10 }}
+          style={{
+            backgroundColor: 'white',
+            borderRadius: 30,
+            height: 40,
+            margin: 10,
+          }}
           onClick={handleClickOpen}
           size="small"
         >
           <CountdownCircleTimer //타이머
-            isPlaying//={isPlay}
+            isPlaying //={isPlay}
             size={35}
             strokeWidth={2}
-            duration={settingTime}
-            initialRemainingTime={currentTime}
-            trailColor={"#FFFFFF"}
-            colors={"#FF0000"}
+            duration={getSettingTime()}
+            initialRemainingTime={getRemaingTime()}
+            trailColor={'#FFFFFF'}
+            colors={'#FF0000'}
           >
             {remainTime}
             {/* {{({ remainingTime }) => remainingTime}} */}
@@ -125,10 +175,10 @@ export default function Timer(): JSX.Element {
                 isPlaying
                 size={400}
                 strokeWidth={20}
-                duration={settingTime}
-                initialRemainingTime={currentTime}
-                trailColor={"#FFFFFF"}
-                colors={"#FF0000"}
+                duration={getSettingTime()}
+                initialRemainingTime={getRemaingTime()}
+                trailColor={'#FFFFFF'}
+                colors={'#FF0000'}
               >
                 {remainTime}
               </CountdownCircleTimer>
@@ -142,20 +192,20 @@ export default function Timer(): JSX.Element {
                   type="number"
                   onChange={handleChangeMinutes}
                   autoFocus={true}
-                  endAdornment={<InputAdornment position="end">분</InputAdornment>}
+                  endAdornment={
+                    <InputAdornment position="end">분</InputAdornment>
+                  }
                 />
               </FormControl>
-              <Button onClick={handleSet} color="primary" >
+              <Button onClick={handleSet} color="primary">
                 Set
-                </Button>
+              </Button>
             </div>
           </div>
         </Modal>
       </div>
     );
-  }
+  };
 
-  return (
-    <CountdownTimer />
-  )
+  return <CountdownTimer />;
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 // 브라우저 라우팅 라이브러리
-import { BrowserRouter, Switch, Route, Redirect, Router } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+  Router,
+} from 'react-router-dom';
 
 // axios 설정 파일
 import { configure } from 'axios-hooks';
@@ -24,80 +30,77 @@ import cookie from 'react-cookies';
 import UserContext, { useUser } from './utils/contexts/UserContext';
 
 function Index(): JSX.Element {
-
   // api 요청 axios 설정 적용
   configure({ axios: axios.axiosInstance });
 
   // 앱 전체 테마 정의
   const THEME = createMuiTheme({
     ...globalTheme,
-    palette: { ...globalTheme.palette }
+    palette: { ...globalTheme.palette },
   });
 
   const {
-    user, handleLogout, handleProfile, state,
-    handleAutoLogin
+    user,
+    handleLogout,
+    handleProfile,
+    state,
+    handleAutoLogin,
   } = useUser();
 
   // 페이지 렌더링 -> access Token 및 refresh Token 확인
   React.useEffect(() => {
     /* uuid 가 쿠키에 존재  */
     if (cookie.load('uuid')) {
-      window.localStorage.clear();
+      //window.localStorage.clear();
       window.localStorage.removeItem('uuid');
 
       /* 로컬 스토리지에 삽입  */
-      window.localStorage.setItem('uuid', cookie.load('uuid'))
+      window.localStorage.setItem('uuid', cookie.load('uuid'));
     }
 
     /* access token 이 쿠키에 존재 */
-    if (cookie.load('accessToken')) { // accessToken 을 axios 디폴트 요청 헤더에 삽입해야한다.
+    if (cookie.load('accessToken')) {
+      // accessToken 을 axios 디폴트 요청 헤더에 삽입해야한다.
       axios.setAxiosHeaders('accesstoken', cookie.load('accessToken'));
       handleProfile();
     } else if (!cookie.load('accessToken')) {
       axios.setAxiosHeaders('accesstoken', null);
     }
-
   }, [handleProfile]);
 
   React.useEffect(() => {
     if (!cookie.load('accessToken') && localStorage.getItem('uuid'))
       handleAutoLogin();
-  }, [])
+  }, []);
 
   return (
     <ThemeProvider theme={THEME}>
       {/* 앱 전체 테마 설정 */}
       {/* 유저 컨택스트 제공자 설정 */}
-      <UserContext.Provider value={{
-        user, handleLogout, handleProfile, state,
-        handleAutoLogin
-      }}>
+      <UserContext.Provider
+        value={{
+          user,
+          handleLogout,
+          handleProfile,
+          state,
+          handleAutoLogin,
+        }}
+      >
         <BrowserRouter>
-
           {/* 상단 고정 앱 바 */}
           <LayoutAppBar />
 
           {/* 라우팅 스위치 */}
           <Switch>
-
             {/* 메인 페이지 */}
             <Route path="/" component={Main} />
-
           </Switch>
-
         </BrowserRouter>
-
       </UserContext.Provider>
-
-
     </ThemeProvider>
   );
 }
 
-ReactDOM.render(
-  <Index />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 serviceWorker.unregister();
