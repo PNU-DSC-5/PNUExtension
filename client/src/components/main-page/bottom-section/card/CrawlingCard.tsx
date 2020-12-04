303d97278eb7eadee014fd621522bcee3d11b582
RINA :D <computer7214@gmail.com>
2020-12-04 16:12:33 +0900

    해커톤 context column도 표시
    해커톤(contest) table에는 date column 대신 context가 존재해서 이것도 받아와서 출력했습니다.
    이미지도 데이터가 존재하면 받아올 수 있도록 했습니다.

--- client/src/components/main-page/bottom-section/card/CrawlingCard.tsx
+++ client/src/components/main-page/bottom-section/card/CrawlingCard.tsx
@@ -37,17 +37,22 @@ const CrawlingCard = (input: Props) => {
   return (
     <Cards className={classes.section}>
       <CardActionArea href={input.card.href}>
-        {/* <CardMedia // 사진 - 어떻게 설정하지?
-          component="img"
-          alt="Contemplative Reptile"
-          height="240"
-          // image="/images/space.jpg"
-          title="Space"
-        /> */}
+        {
+          <CardMedia
+            component="img"
+            alt={input.card.title}
+            height="240"
+            image={input.card.href}
+            title={input.card.title}
+          />
+        }
         <Typography variant="caption">{input.card.category}</Typography>
         <CardContent style={{ alignSelf: "flex-end" }}>
           <Typography gutterBottom variant="h6" component="h2">
             {input.card.title}
+          </Typography>
+          <Typography variant="body2" component="p">
+            {input.card.context}
           </Typography>
           <Typography variant="body2" component="p">
             {input.card.date}
