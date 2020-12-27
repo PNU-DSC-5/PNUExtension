9a33736fe6f9c00b14f0f97d9b6ef995e5de612f
RINA :D <computer7214@gmail.com>
2020-12-27 20:06:22 +0900

    TODO
    TODO 기능을 추가했습니다.
    TODO Item을 입력받을 때 영어는 문제없이 잘되는데 한글은 이상하게 받아와집니다. 수정이 필요합니다.

--- client/src/components/main-page/bottom-section/to-do/Todo.tsx
+++ client/src/components/main-page/bottom-section/to-do/Todo.tsx
@@ -1,20 +1,127 @@
-import React from 'react';
+import React from "react";
+import TodoDialog from "./TodoDialog";
 
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
+import DoneIcon from "@material-ui/icons/Done";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import CreateIcon from "@material-ui/icons/Create";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Button from "@material-ui/core/Button";
+import Dialog, { DialogProps } from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { Typography } from "@material-ui/core";
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  section: {
-    height: '100%',
-    width: '100%',
-    textAlign: 'center',
-    backgroundColor: theme.palette.secondary.main,
-    borderRadius: 8,
-  },
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    section: {
+      height: "100%",
+      width: "100%",
+      textAlign: "center",
+      backgroundColor: theme.palette.secondary.main,
+      borderRadius: 8,
+    },
+    icon: {
+      width: 20,
+      height: 20,
+    },
+  })
+);
 
 export default function Todo(): JSX.Element {
   const classes = useStyles();
-  return <Paper className={classes.section}>Todo</Paper>;
+
+  const [data, setData] = React.useState([
+    "item 1",
+    "item 2",
+    "item 3",
+    "item 4",
+  ]);
+
+  const [checked, setChecked] = React.useState([0]);
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState<DialogProps["scroll"]>("paper");
+  const handleClickOpen = (scrollType: DialogProps["scroll"]) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  return (
+    <Paper className={classes.section}>
+      <Button onClick={handleClickOpen("paper")}>
+        <Typography
+          variant="body1"
+          align="center"
+          color="textPrimary"
+          style={{ fontWeight: "bold" }}
+        >
+          TODO
+        </Typography>
+      </Button>
+      <List>
+        {data.map((value, index) => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return (
+            <ListItem
+              key={value}
+              role={undefined}
+              dense
+              button
+              onClick={handleToggle(index + 1)}
+              style={{ height: 26 }}
+            >
+              <ListItemIcon>
+                <DoneIcon />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`${value.slice(0, 15)}`} />
+              <ListItemSecondaryAction>
+                <IconButton
+                  onClick={(e) => {
+                    let newData: string[] = [];
+                    for (let i = 0; i < data.length; i++) {
+                      if (i != index) newData.push(data[i]);
+                    }
+                    setData(newData);
+                  }}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          );
+        })}
+      </List>
+      <TodoDialog
+        inputData={data}
+        setOriginData={setData}
+        open={open}
+        setOpen={setOpen}
+      />
+    </Paper>
+  );
 }
