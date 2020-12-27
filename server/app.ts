dfbd5da46f2126fe8f53b01a99787c11104899b9
KIMBEOBWOO <qjqdn1568@naver.com>
2020-12-27 18:13:03 +0900

    test

--- server/app.ts
+++ server/app.ts
@@ -31,6 +31,22 @@ class PNUApi {
 
   private initializeAppSettings(): void {
     this.app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+    /* http 통신 origin Url 설정 -> cors 옵션 설정 */
+    const whiteList = [
+      "http://localhost:3003", 
+      "http://localhost:3000",
+      "https://front-dot-pnuextension.dt.r.appspot.com",
+    ];
+
+    const corsOptions = {
+      origin: whiteList,
+      credentials: true
+    };
+
+    this.app.use(cors(corsOptions));
+
     this.app.use((req, res, next) => {
       res.setHeader("Access-Control-Allow-Origin", "*");
       res.setHeader(
@@ -43,20 +59,6 @@ class PNUApi {
       );
       next();
     });
-
-    /* http 통신 origin Url 설정 -> cors 옵션 설정 */
-    const whiteList = [
-      "http://localhost:3003", 
-      "http://localhost:3000",
-      "https://front-dot-pnuextension.dt.r.appspot.com"
-    ];
-
-    const corsOptions = {
-      origin: whiteList,
-      credentials: true
-    };
-
-    this.app.use(cors(corsOptions));
 
     this.app.use(express.json());
     this.app.use(bodyParser.json());
